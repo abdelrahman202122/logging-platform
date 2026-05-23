39e503754f6f2bc536d9be65931c8e0ba0d50df1
Abdelrahman <abdoezzat098@gmail.com>
2026-05-23 19:37:51 +0300

    adding server folder structure

---
+++ apps/server/eslint.config.js
@@ -0,0 +1,27 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.js'],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'no-undef': 'error',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+];
