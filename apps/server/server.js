e1c988256bd713c07d5388a279244348d038ff95
Abdelrahman <abdoezzat098@gmail.com>
2026-05-25 19:30:22 +0300

    inspecting deployment issue

--- apps/server/server.js
+++ apps/server/server.js
@@ -3,7 +3,14 @@ const { connectDatabase } = require('./config/database');
 const { env } = require('./config/env');
 
 const startServer = async () => {
+  console.log('Starting server...');
+  console.log('NODE_ENV:', env.nodeEnv);
+  console.log('PORT:', env.port);
+  console.log('Connecting to database...');
+
   await connectDatabase();
+
+  console.log('Database connected successfully');
 
   app.listen(env.port, () => {
     console.log(`Server running on port ${env.port}`);
